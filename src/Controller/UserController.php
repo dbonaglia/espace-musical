537947bb36dd12d479df1747c3fcc91f4f1bed98
Wabon95 <dbonaglia95@protonmail.com>
2019-08-29 19:56:18 +0200

    UserController optimisation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -17,13 +17,16 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /** @Route("/api-db/users") */
 class UserController extends AbstractController {
-
     /** @Route("/add") */
-    public function add(Request $request, UserPasswordEncoderInterface $encoder, ValidatorInterface $validator, ObjectManager $manager, SerializerInterface $serializer) {
-
+    public function add(
+        Request $request,
+        UserPasswordEncoderInterface $encoder,
+        ValidatorInterface $validator,
+        ObjectManager $manager,
+        SerializerInterface $serializer
+        ) {
         // On récupère les données Json sous forme de tableau PHP
         $data = json_decode($request->getContent(), true);
-        
         // On insère les données Json dans une nouvelle instance de l'entité User
         $user = new User();
         $user
@@ -31,13 +34,11 @@ class UserController extends AbstractController {
             ->setUsername($data['username'])
             ->setPassword($data['password'])
         ;
-
         // On vérifie les contraintes de validation
         $errors = $validator->validate($user);
-
         // On envoie la réponse après vérification des erreurs possible
         if(count($errors) > 0) {
-            return APIController::responseJson($serializer->serialize($errors, 'json'), Response::HTTP_I_AM_A_TEAPOT);
+            return APIController::responseJson($serializer->serialize($errors, 'json'), Response::HTTP_PRECONDITION_FAILED);
         } else {
             $user->setPassword($encoder->encodePassword($user, $data['password']));
             $manager->persist($user);
@@ -48,7 +49,6 @@ class UserController extends AbstractController {
 
     /** @Route("/connect") */
     public function connect(Request $request, SerializerInterface $serializer) {
-
         if($user = $this->getUser()) {
             $user->setPassword(''); // Pour raison de sécurité, on évite de renvoyer le mdp au front
             return APIController::responseJson($serializer->serialize($user, 'json'), Response::HTTP_OK);
@@ -57,32 +57,34 @@ class UserController extends AbstractController {
 
     /** @Route("/edit") */
     public function edit(Request $request, UserRepository $ur, UserPasswordEncoderInterface $encoder, ObjectManager $manager, ValidatorInterface $validator) {
-
         // On récupère les données Json sous forme de tableau PHP
         $data = json_decode($request->getContent(), true);
-
-        // Si un utilisateur avec cette adresse emai lest trouvé en BDD
+        // Si un utilisateur avec cette adresse email est trouvé en BDD
         if($user = $ur->findOneByEmail($data['email'])) {
-
             // Si son mot de passe actuel est correct
             if(password_verify($data['password'], $user->getPassword())) {
                 $modifs = false;
+                $passwordModif = false;
                 // S'il souhaite modifier son mot de passe
                 if(array_key_exists('passwordNew', $data) || array_key_exists('passwordNewConfirm', $data)) {
                     if($data['passwordNew'] === $data['passwordNewConfirm']) {
-                        $user->setPassword($encoder->encodePassword($user, $data['password']));
-                        $modifs = true;
+                        if(!password_verify($data['passwordNew'], $user->getPassword())) {
+                            $user->setPassword($data['passwordNew']);
+                            $passwordModif = true;
+                            $modifs = true;
+                        } else {
+                            return new Response('Votre nouveau mot de passe est identique à l\'ancien.', Response::HTTP_PRECONDITION_FAILED);
+                        }
                     } else {
-                        return new Response('Les nouveaux mot de passe ne correspondent pas.', Response::HTTP_I_AM_A_TEAPOT);
+                        return new Response('Les nouveaux mot de passe ne correspondent pas.', Response::HTTP_PRECONDITION_FAILED);
                     }
                 }
-
                 if(array_key_exists('emailnew', $data)) {
                     if($data['emailnew'] != $user->getEmail()) {
                         $user->setEmail($data['emailnew']);
                         $modifs = true;
                     } else {
-                        return new Response('Votre nouvelle adresse email est identique à l\'ancienne.', Response::HTTP_I_AM_A_TEAPOT);
+                        return new Response('Votre nouvelle adresse email est identique à l\'ancienne.', Response::HTTP_PRECONDITION_FAILED);
                     }
                 }
                 if(array_key_exists('username', $data)) {
@@ -90,7 +92,7 @@ class UserController extends AbstractController {
                         $user->setUsername($data['username']);
                         $modifs = true;
                     } else {
-                        return new Response('Votre pseudo est identique à l\'ancien.', Response::HTTP_I_AM_A_TEAPOT);
+                        return new Response('Votre pseudo est identique à l\'ancien.', Response::HTTP_PRECONDITION_FAILED);
                     }
                 }
                 if(array_key_exists('firstname', $data)) {
@@ -98,7 +100,7 @@ class UserController extends AbstractController {
                         $user->setFirstname($data['firstname']);
                         $modifs = true;
                     } else {
-                        return new Response('Votre prénom est identique à l\'ancien.', Response::HTTP_I_AM_A_TEAPOT);
+                        return new Response('Votre prénom est identique à l\'ancien.', Response::HTTP_PRECONDITION_FAILED);
                     }
                 }
                 if(array_key_exists('lastname', $data)) {
@@ -106,7 +108,7 @@ class UserController extends AbstractController {
                         $user->setLastname($data['lastname']);
                         $modifs = true;
                     } else {
-                        return new Response('Votre nom est identique à l\'ancien.', Response::HTTP_I_AM_A_TEAPOT);
+                        return new Response('Votre nom est identique à l\'ancien.', Response::HTTP_PRECONDITION_FAILED);
                     }
                 }
                 if(array_key_exists('zipCode', $data)) {
@@ -114,7 +116,7 @@ class UserController extends AbstractController {
                         $user->setZipCode($data['zipCode']);
                         $modifs = true;
                     } else {
-                        return new Response('Votre code postal est identique à l\'ancien.', Response::HTTP_I_AM_A_TEAPOT);
+                        return new Response('Votre code postal est identique à l\'ancien.', Response::HTTP_PRECONDITION_FAILED);
                     }
                 }
                 if(array_key_exists('address', $data)) {
@@ -122,29 +124,28 @@ class UserController extends AbstractController {
                         $user->setAddress($data['address']);
                         $modifs = true;
                     } else {
-                        return new Response('Votre adresse est identique à l\'ancienne.', Response::HTTP_I_AM_A_TEAPOT);
+                        return new Response('Votre adresse est identique à l\'ancienne.', Response::HTTP_PRECONDITION_FAILED);
                     }
                 }
-
                 // On vérifie les contraintes de validation
                 $errors = $validator->validate($user);
-
                 // On envoie la réponse après vérification des erreurs possible
                 if(count($errors) > 0) {
-                    return new Response($errors, Response::HTTP_I_AM_A_TEAPOT);
-                } else if ($modifs) {
+                    return new Response($errors, Response::HTTP_PRECONDITION_FAILED);
+                } elseif ($modifs) {
+                    if($passwordModif) $user->setPassword($encoder->encodePassword($user, $data['passwordNew']));
                     $user->setUpdatedAt(new \Datetime());
                     $manager->persist($user);
                     $manager->flush();
                     return new Response('L\'utilisateur à correctement été modifié.', Response::HTTP_OK);
-                } else {
-                    return new Response('Vous n\'avez rien modifié', Response::HTTP_I_AM_A_TEAPOT);
+                } elseif(!$modifs) {
+                    return new Response('Vous n\'avez rien modifié', Response::HTTP_OK);
                 }
             } else {
-                return new Response('Votre mot de passe n\'est pas corect', Response::HTTP_I_AM_A_TEAPOT);
+                return new Response('Votre mot de passe n\'est pas correct.', Response::HTTP_UNAUTHORIZED );
             }
         } else {
-            return new Response('Aucun utilisateur avec cette adresse email trouvé.', Response::HTTP_I_AM_A_TEAPOT);
+            return new Response('Aucun utilisateur possédant cette adresse email.', Response::HTTP_UNAUTHORIZED );
         }
     }
 }
